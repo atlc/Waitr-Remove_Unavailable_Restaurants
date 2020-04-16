cbd986fa892309d93eaec93dd9220de7b9ef8e69
Andrew Lloyd Cartwright <andrewlloydcartwright+atlc@gmail.com>
2020-04-16 12:11:54 -0500

    Update Greasemonkey.js

--- Greasemonkey.js
+++ Greasemonkey.js
@@ -13,13 +13,4 @@ setTimeout(() => {
   let seeMoreButt = [...document.getElementsByTagName('button')].filter(b => b.innerText === "See More Restaurants")[0];
   seeMoreButt.onclick = (e) => setTimeout(() => removeUnavailableSections(), 250);
   removeUnavailableSections();
-  
-  // Hijack the blog button at the top of the page to manually remove the elements? Think above is better UX though
-  /*
-    let navItems = document.querySelector("[class*='MainMenu']").firstElementChild;
-    let blogButton = navItems.children[3];
-    blogButton.firstElementChild.addEventListener("click", (e) => e.preventDefault());
-    blogButton.firstElementChild.innerText = "Remove Unavailable";
-    blogButton.onclick = removeUnavailableSections;
-  */
-}, 2000);
+}, 3000);
