d4a82843d5a8e4e6c7a37ed431f14ec7ff45c5b2
Andrew Lloyd Cartwright <andrewlloydcartwright+atlc@gmail.com>
2020-04-16 16:50:51 -0500

    Update Greasemonkey.js

--- Greasemonkey.js
+++ Greasemonkey.js
@@ -12,5 +12,12 @@ setTimeout(() => {
   let removeUnavailableSections = () => [...document.querySelectorAll("section>div>div>a>div>h3[class*='ComingSoon']")].forEach(unavailable=>unavailable.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.removeChild(unavailable.parentElement.parentElement.parentElement.parentElement.parentElement));
   let seeMoreButt = [...document.getElementsByTagName('button')].filter(b => b.innerText === "See More Restaurants")[0];
   seeMoreButt.onclick = (e) => setTimeout(() => removeUnavailableSections(), 250);
+
+  let navItems = document.querySelector("[class*='MainMenu']").firstElementChild;
+  let blogButton = navItems.children[3];
+  blogButton.firstElementChild.addEventListener("click", (e) => e.preventDefault());
+  blogButton.firstElementChild.innerText = "Remove Unavailable";
+  blogButton.onclick = removeUnavailableSections;
+  
   removeUnavailableSections();
 }, 3000);
