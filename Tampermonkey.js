9c49f000493760a0621a63417f00f4523bebf88d
Andrew Lloyd Cartwright <andrewlloydcartwright+atlc@gmail.com>
2020-04-16 12:12:13 -0500

    Create Tampermonkey.js

---
+++ Tampermonkey.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Remove Waitr "Unavailable" Sections
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  Give unavailable restaurants the boot. Makes Waitr a lot less annoying in relative food deserts.
+// @author       https://github.com/atlc
+// @match        http://waitrapp.com
+// @match        http://waitrapp.com/*
+// @match        https://waitrapp.com
+// @match        https://waitrapp.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    setTimeout(() => {
+        let removeUnavailableSections = () => [...document.querySelectorAll("section>div>div>a>div>h3[class*='ComingSoon']")].forEach(unavailable=>unavailable.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.removeChild(unavailable.parentElement.parentElement.parentElement.parentElement.parentElement));
+        let seeMoreButt = [...document.getElementsByTagName('button')].filter(b => b.innerText === "See More Restaurants")[0];
+        seeMoreButt.onclick = (e) => setTimeout(() => removeUnavailableSections(), 250);
+        removeUnavailableSections();
+    }, 3000);
+})();
